59c354c6151c61291dc62097159641aa1293f220
Razvan Baraboi <razvan@noom.com>
2021-01-27 14:43:17 +0200

    Switched to test plans

--- NotesUITests/NotesUITests.swift
+++ NotesUITests/NotesUITests.swift
@@ -32,5 +32,9 @@ class NotesUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+
+    func testTwo() {
+        
+    }
     
 }
